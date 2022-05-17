f03e153f1c5cf8f720d6435311321bbf6797042a
Bububuka <savinkova.vika@mail.ru>
2022-05-17 23:49:15 +0500

    Initial

--- BonusMilesService.java
+++
@@ -1,9 +0,0 @@
-public class BonusMilesService {
-    public int calculate (int cost){
-            int costForMile = 20;
-            int bonus = cost / costForMile;
-        return bonus;
-    }
-    }
-
-
---
+++ src/BonusMilesService.java
@@ -0,0 +1,11 @@
+package src;
+
+public class BonusMilesService {
+    public int calculate(int cost) {
+        int costForMile = 20;
+        int bonus = cost / costForMile;
+        return bonus;
+    }
+}
+
+
